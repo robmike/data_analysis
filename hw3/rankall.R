bb567a26694ff6e136a276cf11e3b98a0dfd9e49
invalid <invalid@invalid>
2013-01-20 14:23:00 -0500

    hw3: rankall completed

--- hw3/rankall.R
+++ hw3/rankall.R
@@ -23,27 +23,37 @@ rankall <- function(outcome, num = "best") {
   # x <- transform(x, as.numeric(colname))
   ## x[x[,colname] == 'Not Available',colname] <- NA
   ## x <- na.omit(x)
-  if(num == 'best') {
-    num <- 1
-  }
-  else if(num == 'worst') {
-    num <- nitems
-  }
 
   rankall_helper <- function(x) {
-    ii <- order(x[2], x[1], na.last=NA, decreasing=FALSE)
-    nitems <- length(ii)
-    if(num > nitems) {
-      return(NA)
+    ii <- order(x[,colname], x$Hospital.Name, na.last=NA, decreasing=FALSE)
+    # Return NA hospital name if fewer than num hospitals in state
+    if(is.numeric(num) & num > nrow(x)) {       #FIXME: Are always guaranteed one entry?
+      out <- x[1,]
+      out[,'Hospital.Name'] <- NA
     }
-    as.character(x[[ii[num],1]])
+    else {
+      nitems <- length(ii)
+      if(num == 'best') {
+        num <- 1
+      }
+      else if(num == 'worst') {
+        num <- nitems
+      }
+      out <- x[ii[num],]
+      out[,'Hospital.Name'] <- as.character(out[,'Hospital.Name'])
+    }
+    out <- out[,c('Hospital.Name', 'State')]
+    colnames(out) = c('hospital','state')
+    out
   }
 
-  
-
   # aggregate(x, list(x$State), rankall_helper)
-  states <- unique(x$State)
+  # states <- unique(x$State)
   # lapply(split(x, x$State), rankall_helper)
-  unsplit(lapply(split(x, x$State), rankall_helper), states)
+  # do.call(rbind, lapply(split(x, x$State), rankall_helper))'
+  #by(x, x$State, rankall_helper)
+  do.call(rbind, by(x, x$State, rankall_helper))
+  ## do.call(rbind, by(x, x$State, function(x)
+  ##         x[order(x[,colname], x$Hospital.Name),c('Hospital.Name', 'State')][num,]))
 }
 
