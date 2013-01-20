f4fc9ec95fa7c4ee303a22962d779136b308ae18
invalid <invalid@invalid>
2013-01-20 17:12:13 -0500

    hw3: part3 wip

---
+++ hw3/part3.R
@@ -0,0 +1,19 @@
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+outcome[, 11] <- as.numeric(outcome[, 11])
+
+thresh <- 20
+x <- table(outcome$State)
+x <- x[x > thresh]
+outcome2 <- outcome[outcome$State %in% names(x),]
+
+death <- outcome2[, 11]
+state <- (outcome2$State
+## boxplot(death ~ state,
+##         ylab = '30-Day Death Rate',
+##         title = 'Heart Attack 30-day Death Rate by State',
+##         las = 2)
+
+boxplot(death ~ state,
+        ylab = '30-Day Death Rate',
+        las = 2)
+title('Heart Attack 30-day Death Rate by State')
